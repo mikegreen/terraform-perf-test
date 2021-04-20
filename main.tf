f00972a4cbcb85b03fde85433187e952cfb4eb74
Mike Green <772413+mikegreen@users.noreply.github.com>
2021-04-20 15:07:12 -0600

    updates

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = "> 0.13"
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "greenacreslimited"
+
+    workspaces {
+      name = "terraform-perf-test"
+    }
   }
 }
